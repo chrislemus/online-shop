0b0780beadc9174e13f089c349dc197eb5370383
chrislemus <icrislemus@gmail.com>
2021-09-20 14:13:57 -0400

    add rootDir util

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  const filePath = path.join(__dirname, '../', 'views', 'add-product.html');
+  const filePath = path.join(rootDir, 'views', 'add-product.html');
   res.sendFile(filePath);
 });
 
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
   res.redirect('/');
 });
 
