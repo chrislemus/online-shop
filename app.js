870b3ce54884843427b60671ceaab4573d88e9f4
chrislemus <icrislemus@gmail.com>
2021-09-20 19:37:24 -0400

    create all views with .ejs engine/format

--- app.js
+++ app.js
@@ -2,18 +2,9 @@ const express = require('express');
 const path = require('path');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const expressHbs = require('express-handlebars');
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  })
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,7 +13,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: null });
 });
 
 app.listen(3000);
