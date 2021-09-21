5376a75d5d17acad8b01795e0a8186d10450038a
chrislemus <icrislemus@gmail.com>
2021-09-20 20:42:26 -0400

    persist products data by storing in file

--- models/product.js
+++ models/product.js
@@ -1,15 +1,36 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, data) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(data));
+    }
+  });
+};
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (error) => {
+        console.log(error);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
