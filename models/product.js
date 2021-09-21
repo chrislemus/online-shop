321f94cbd1318b7afb39e5d7138683813d1d5130
chrislemus <icrislemus@gmail.com>
2021-09-20 21:05:38 -0400

    add additional routes and restructure project

--- models/product.js
+++ models/product.js
@@ -2,30 +2,34 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-  path.dirname(require.main.filename),
+  path.dirname(process.mainModule.filename),
   'data',
   'products.json'
 );
 
 const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, data) => {
+  fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
     } else {
-      cb(JSON.parse(data));
+      cb(JSON.parse(fileContent));
     }
   });
 };
+
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
     getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (error) => {
-        console.log(error);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
       });
     });
   }
