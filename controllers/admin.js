113af1439ac35e4c7a941cddce47d6b6f37f2aa0
chrislemus <icrislemus@gmail.com>
2021-09-21 16:55:50 -0400

    Product.save saves new product or updates existing product

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -29,6 +29,13 @@ exports.getEditProduct = (req, res, next) => {
       product,
     });
   });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const { productId, title, price, imageUrl, description } = req.body;
+  const product = new Product(productId, title, imageUrl, description, price);
+  product.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
