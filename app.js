d4b624eaa94798194f8e892cc610c37cdd83d16f
chrislemus <icrislemus@gmail.com>
2021-09-22 17:28:40 -0400

    add order and orderItem sequelize models

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+
 const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -35,6 +39,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
   // .sync({ force: true })
