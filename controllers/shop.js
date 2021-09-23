3f691260908eb376319fe32e629b813c0a9a3f3a
chrislemus <icrislemus@gmail.com>
2021-09-23 17:55:12 -0400

    display items in cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -61,8 +61,9 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then((products) => {
+    .populate('cart.items.productId')
+    .then((user) => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
