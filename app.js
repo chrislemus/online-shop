9d0eb1d6ffd7ee64f181e1e9c7e2570667545387
chrislemus <icrislemus@gmail.com>
2021-09-23 19:36:30 -0400

    add login page and styling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const path = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const app = express();
@@ -23,6 +24,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
