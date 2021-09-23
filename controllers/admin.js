bb4830dc72caf179a3e0dc3baa75c891f308cb5c
chrislemus <icrislemus@gmail.com>
2021-09-23 11:02:21 -0400

    postEditProduct connects to mongodb instead of sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,43 +10,37 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const { title, price, description, imageUrl } = req.body;
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product(title, price, imageUrl, description);
   product
     .save()
     .then(() => res.redirect('/admin/products'))
     .catch((err) => console.log(err));
 };
 
-// exports.getEditProduct = (req, res, next) => {
-//   req.user
-//     .getProducts({ where: { id: req.params.productId } })
-//     .then(([product]) => {
-//       if (!product) return res.redirect('/');
-//       res.render('admin/edit-product', {
-//         pageTitle: 'Edit Product',
-//         path: '/admin/edit-product',
-//         editing: true,
-//         product,
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.getEditProduct = (req, res, next) => {
+  Product.findById(req.params.productId)
+    .then((product) => {
+      if (!product) return res.redirect('/');
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: true,
+        product,
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
-// exports.postEditProduct = (req, res, next) => {
-//   const { productId, title, price, imageUrl, description } = req.body;
-//   Product.findByPk(productId)
-//     .then((product) => {
-//       product.title = title;
-//       product.price = price;
-//       product.imageUrl = imageUrl;
-//       product.description = description;
-//       return product.save();
-//     })
-//     .then(() => {
-//       res.redirect('/admin/products');
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.postEditProduct = (req, res, next) => {
+  const { productId, title, price, imageUrl, description } = req.body;
+  const product = new Product(title, price, imageUrl, description, productId);
+  product
+    .save()
+    .then(() => {
+      res.redirect('/admin/products');
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
--- models/product.js
+++ models/product.js
@@ -2,24 +2,27 @@ const mongodb = require('mongodb');
 const { getDb } = require('../util/database');
 
 module.exports = class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp.catch((err) => {
+      console.log(err);
+    });
   }
 
   static fetchAll() {
