1f2c0c07dcdd5d128f535ca546bf54e0973ea2de
chrislemus <icrislemus@gmail.com>
2021-09-24 19:32:22 -0400

    add csrf protection to all post request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 require('dotenv').config();
 const path = require('path');
 const adminRoutes = require('./routes/admin');
@@ -9,6 +10,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
+
 const MONGODB_URI = `mongodb+srv://adminUser:${encodeURIComponent(
   process.env.MONGO_DB_PASSWORD
 )}@cluster0.rwx03.mongodb.net/nodejs-online-shop?retryWrites=true&w=majority`;
@@ -18,6 +20,7 @@ const mongoDbStore = new MongoStore({
   uri: MONGODB_URI,
   collection: 'sessions',
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 
@@ -31,6 +34,8 @@ app.use(
     store: mongoDbStore,
   })
 );
+//its important to add middleware after session middleware above
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -42,6 +47,14 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => console.log(err));
+});
+
+//important to add the following code after user session is set
+//but before the routes
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use('/admin', adminRoutes);
